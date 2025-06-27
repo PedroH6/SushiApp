042aa1b5605aa3ebd2bdb18da1190db41443e3f0
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-06-27 15:00:31 -0300

    adicionando padding e image na intro_page

--- lib/intro_page.dart
+++ lib/intro_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -7,6 +8,34 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 138, 60, 55),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 25),
+
+            //Shop name
+            Text(
+              'SUSHI MAN',
+              style: GoogleFonts.dmSerifDisplay(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            //icon
+            Image.asset('lib/images/salmon_eggs.png'),
+
+            //title
+
+            //subtitle
+
+            //get start button
+          ],
+        ),
+      ),
     );
   }
 }
