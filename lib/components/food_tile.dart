1a5ae7aea889dfb6a356c8f372a4c35b84034bc8
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-07-01 14:09:34 -0300

    criando o component food_tile

---
+++ lib/components/food_tile.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class FoodTile extends StatelessWidget {
+  final Food food;
+  const FoodTile({super.key, required this.food});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        children: [
+          //image
+
+          //text
+
+          //price + rating
+        ],
+      ),
+    );
+  }
+}
